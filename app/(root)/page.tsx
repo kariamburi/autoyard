093c1d10e2f39233321ff4e1983ad64ed6f456e2
Paul Irungu <paul.irungu@gmail.com>
2024-08-19 16:30:20 +0300

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,95 @@
+import Header from "@/components/shared/Header";
+import dynamic from "next/dynamic";
 import Image from "next/image";
+import Skeleton from "@mui/material/Skeleton";
+import { SearchParamProps } from "@/types";
+import { auth } from "@clerk/nextjs/server";
+import { getAllCategories } from "@/lib/actions/category.actions";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+//import Collection from "@/components/shared/Collection";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import Link from "next/link";
+const Menumobile = dynamic(() => import("@/components/shared/Menumobile"), {
+  ssr: false,
+  loading: () => (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="flex flex-wrap mt-10 gap-1 justify-center">
+        <Skeleton
+          variant="rectangular"
+          animation="wave"
+          //  height={100}
+          className="rounded-sm w-[100px] md:w-[150px] lg:w-[150px]"
+        />
+        <Skeleton
+          variant="rectangular"
+          animation="wave"
+          // height={100}
+          className="rounded-sm w-[100px] md:w-[150px] lg:w-[150px]"
+        />
+        <Skeleton
+          variant="rectangular"
+          animation="wave"
+          // height={100}
+          className="rounded-sm w-[100px] md:w-[150px] lg:w-[150px]"
+        />
+        <Skeleton
+          variant="rectangular"
+          animation="wave"
+          // height={100}
+          className="rounded-sm w-[100px] md:w-[150px] lg:w-[150px]"
+        />
+      </div>
+    </div>
+  ),
+});
+export default async function Home({ searchParams }: SearchParamProps) {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+  const userName = sessionClaims?.userName as string;
+  const userImage = sessionClaims?.userImage as string;
 
-export default function Home() {
-  return <div>Home irungu thumvi</div>;
+  const categoryList = await getAllCategories();
+
+  return (
+    <main>
+      <div className="bg-gradient-to-r from-emerald-800 to-emerald-950">
+        <Header />
+      </div>
+
+      <div className="max-w-6xl mx-auto flex mt-2 ">
+        <div className="flex-1">
+          <div className="p-0">
+            <Menumobile categoryList={categoryList} />
+          </div>
+          <div className="p-2 mt-2">
+            <div className="flex w-full items-center justify-between gap-5 p-2 md:flex-row">
+              <div className="items-center flex">
+                <h2 className="h2-bold p-5">Trending Ads</h2>
+              </div>
+
+              <SignedIn>
+                <Link href="/ads/create">
+                  <button
+                    className={`w-[150px] bg-gradient-to-b from-[#4DCE7A] to-[#30AF5B] hover:bg-[#30AF5B] text-white p-2 rounded-full`}
+                  >
+                    <AddCircleOutlineOutlinedIcon /> SELL
+                  </button>
+                </Link>
+              </SignedIn>
+              <SignedOut>
+                <Link href="/sign-in">
+                  <button
+                    className={`w-[150px] bg-gradient-to-b from-[#4DCE7A] to-[#30AF5B] hover:bg-[#30AF5B] text-white p-2 rounded-lg`}
+                  >
+                    <AddCircleOutlineOutlinedIcon /> SELL
+                  </button>
+                </Link>
+              </SignedOut>
+            </div>
+            COLLECTION
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ components/shared/Menumobile.tsx
@@ -0,0 +1,55 @@
+"use client";
+import qs from "query-string";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+type MobileProps = {
+  categoryList: { imageUrl: string; name: string; _id: string }[];
+};
+export default function Menumobile({ categoryList }: MobileProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const handleCategory = (category: string) => {
+    let newUrl = "";
+    if (category) {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "category",
+        value: category,
+      });
+    } else {
+      newUrl = removeKeysFromQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["category"],
+      });
+    }
+
+    router.push("/category/" + newUrl, { scroll: false });
+  };
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-8 m-1 gap-1">
+        {categoryList.length > 0 &&
+          categoryList.map(
+            (category: { imageUrl: string; name: string; _id: string }) => (
+              <div
+                onClick={() => handleCategory(category.name)}
+                className="h-[100px] bg-white flex flex-col items-center justify-center cursor-pointer rounded-sm p-1 border-0 border-emerald-300 hover:bg-emerald-100 "
+              >
+                <div className="flex flex-col items-center text-center justify-center">
+                  <div className="h-12 w-12 rounded-full bg-white p-2">
+                    <img
+                      className="w-full h-full"
+                      src={category.imageUrl}
+                      alt="Menu Image"
+                    />
+                  </div>
+
+                  <h2 className="text-xs">{category.name}</h2>
+                </div>
+              </div>
+            )
+          )}
+      </div>
+    </div>
+  );
+}
