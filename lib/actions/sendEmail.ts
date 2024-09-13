cf6b1b4d833924b110e0116d8c16792fd7be3530
Paul Irungu <paul.irungu@gmail.com>
2024-09-13 18:13:32 +0300

    first commit

--- lib/actions/sendEmail.ts
+++ lib/actions/sendEmail.ts
@@ -1,18 +1,22 @@
 "use server";
 
-
-import axios from 'axios';
 import nodemailer from 'nodemailer';
-export async function sendemail(recipientEmail: string, phoneNumber:string, message:string,adTitle: string, adUrl:string) {
- 
-   
+
+export async function sendemail(
+  recipientEmail: string, 
+  phoneNumber: string, 
+  message: string, 
+  adTitle: string, 
+  adUrl: string
+) {
+
   let transporter = nodemailer.createTransport({
-    host: 'mail.autoyard.co.ke', // your SMTP host
+    host: process.env.SMTP_HOST, // your SMTP host
     port: 465, // or 465 if SSL
     secure: true, // true for 465, false for other ports
     auth: {
-      user: 'support@autoyard.co.ke', // your email address
-      pass: 'KmPE+_0yZpCaiT/Y', // your email password
+      user: process.env.SMTP_USER, // your email address
+      pass: process.env.SMTP_PASS, // your email password
     },
   });
 
@@ -24,22 +28,14 @@ export async function sendemail(recipientEmail: string, phoneNumber:string, mess
     text: `You have a new inquiry on your ad titled "${adTitle}".\n\nMessage: ${message}\n\nView the ad here: ${adUrl}`, // plain text body
     html: `<p>You have a new inquiry on your ad titled "<b>${adTitle}</b>".</p><p>Message: ${message}</p><p><a href="${adUrl}">View the ad</a></p>`, // HTML body
   };
-  
+
   try {
     // Send the email
     const response = await transporter.sendMail(mailOptions);
-   // console.log(response);
+    console.log('Email sent:', response);
     return "success";
-   // res.status(200).json({ message: 'Email and SMS sent successfully' });
   } catch (error) {
-    console.error('Error sending email or SMS: ', error);
-   // res.status(500).json({ error: 'Failed to send email or SMS' });
+    console.error('Error sending email:', error);
     return "Failed";
-     
   }
-
-
-
-      
-
 }
