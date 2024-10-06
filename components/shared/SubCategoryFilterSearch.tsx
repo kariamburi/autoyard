2e02db737548d09d4cd7b236de97b01c112e561e
Paul Irungu <paul.irungu@gmail.com>
2024-10-06 14:23:35 +0300

    first commit

--- components/shared/SubCategoryFilterSearch.tsx
+++ components/shared/SubCategoryFilterSearch.tsx
@@ -14,7 +14,6 @@ import {
 } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import Tooltip from "@mui/material/Tooltip"; // Import Tooltip from Material-UI
 
 type sidebarProps = {
   category: string;
@@ -59,47 +58,50 @@ const SubCategoryFilterSearch = ({
 
   return (
     <>
-      <Tooltip title="Long press to select a category" arrow>
-        <Select
-          value={searchParams.get("subcategory") ?? "Select Category"}
-          onValueChange={(value: string) => onSelectCategory(value)}
+      <Select
+        value={searchParams.get("subcategory") ?? "Select Category"}
+        onValueChange={(value: string) => onSelectCategory(value)}
+      >
+        <SelectTrigger className="select-field">
+          <SelectValue placeholder="Search Category" />
+        </SelectTrigger>
+        <SelectContent
+          onClick={(e) => e.stopPropagation()} // Prevent click events from propagating
+          onMouseDown={(e) => e.stopPropagation()} // Prevent mouse down events from bubbling up
+          onMouseUp={(e) => e.stopPropagation()} // Prevent mouse up events from bubbling up
+          className="z-20" // Ensure it appears above other elements
         >
-          <SelectTrigger className="select-field">
-            <SelectValue placeholder="Search Category" />
-          </SelectTrigger>
-          <SelectContent
-            onClick={(e) => e.stopPropagation()} // Prevent click events from propagating
-            onMouseDown={(e) => e.stopPropagation()} // Prevent mouse down events from bubbling up
-            onMouseUp={(e) => e.stopPropagation()} // Prevent mouse up events from bubbling up
-            className="z-20" // Ensure it appears above other elements
-          >
-            {categories &&
-              categories
-                .find((cat: any) => cat.name === category)
-                ?.subcategory.map((sub: any, index: number) => (
-                  <SelectItem
-                    key={index}
-                    value={sub.title}
-                    className="flex select-item p-regular-14"
-                  >
-                    <div className="flex w-full gap-1 items-center">
-                      {sub.title}
-                      <div className="text-xs text-emerald-600">
-                        |{" "}
-                        {(AdsCountPerSubcategory &&
-                          AdsCountPerSubcategory.find(
-                            (item: { _id: string; adCount: number }) =>
-                              item._id === sub.title
-                          )?.adCount) ??
-                          0}{" "}
-                        ads
-                      </div>
+          <div className="flex flex-col items-center justify-center bg-green-500 text-white text-xs py-2 rounded-md relative">
+            Long press an option to select a category
+            {/* Arrow pointing down */}
+            <span className="mt-1 w-0 h-0 border-l-[5px] border-r-[5px] border-t-[5px] border-l-transparent border-r-transparent border-t-white"></span>
+          </div>
+          {categories &&
+            categories
+              .find((cat: any) => cat.name === category)
+              ?.subcategory.map((sub: any, index: number) => (
+                <SelectItem
+                  key={index}
+                  value={sub.title}
+                  className="flex select-item p-regular-14"
+                >
+                  <div className="flex w-full gap-1 items-center">
+                    {sub.title}
+                    <div className="text-xs text-emerald-600">
+                      |{" "}
+                      {(AdsCountPerSubcategory &&
+                        AdsCountPerSubcategory.find(
+                          (item: { _id: string; adCount: number }) =>
+                            item._id === sub.title
+                        )?.adCount) ??
+                        0}{" "}
+                      ads
                     </div>
-                  </SelectItem>
-                ))}
-          </SelectContent>
-        </Select>
-      </Tooltip>
+                  </div>
+                </SelectItem>
+              ))}
+        </SelectContent>
+      </Select>
     </>
   );
 };
